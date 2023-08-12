384087d2d21dced10fe52521d99cd75ef77b5198
Ryan <ryangisa2001@gmail.com>
2023-08-12 06:42:29 -0700

    if,else project

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 113 || n == 101)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
