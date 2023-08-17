e4ff53b0efe303ed6b108a600ed7ac8c2ab5a998
Ryan <ryangisa2001@gmail.com>
2023-08-17 13:31:39 -0700

    more functiond

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul -  function that multiplies two integers
+ * @a: is in ASCII table
+ * @b: is in ASCII table
+ *
+ * Return: Addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+	printf("\n");
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ *
+ * Return: Void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
