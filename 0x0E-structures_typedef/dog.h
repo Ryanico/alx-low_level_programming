40d89d9207c707962dfec18ac6e549401a272d0b
Ryanico <ryangisa2001@gmail.com>
2023-09-11 13:03:12 -0700

    structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog info
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
