e05bfb741362752f289347ac0720135b4968d3b6
Ryanico <ryangisa2001@gmail.com>
2023-10-05 07:03:28 -0700

    more malloc project

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _strlen(char *s);
 int is_digit(char *s);
-void errors(void);
+void error(void);
 
 #endif
